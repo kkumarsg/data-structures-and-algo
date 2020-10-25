e18ce021425d63c928bf6e2ca556adb9c7bbd4fb
keerthikumar.sg <keerthikumar.sg@flipkart.com>
2020-10-25 10:18:18 +0530

    appender

--- src/main/java/linkedlists/LinkedListUtil.java
+++ src/main/java/linkedlists/LinkedListUtil.java
@@ -2,12 +2,14 @@ package linkedlists;
 
 public class LinkedListUtil {
 
+    public static final String APPENDER = "->";
+
     public static void printLinkedList(Node head){
         Node iterator = head;
         while(iterator!=null){
             System.out.print(iterator.data);
             if (iterator.next!=null) {
-                System.out.print("->");
+                System.out.print(APPENDER);
             }
             iterator = iterator.next;
         }
